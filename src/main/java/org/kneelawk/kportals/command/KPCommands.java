09734aa0143d84c2737cef57a5c2e0e7b075b0cb
Kneelawk <jedidiah.pommert@gmail.com>
2017-02-02 08:43:30 -0800

    Begin of gui command

--- src/main/java/org/kneelawk/kportals/command/KPCommands.java
+++ src/main/java/org/kneelawk/kportals/command/KPCommands.java
@@ -1,7 +1,18 @@
 package org.kneelawk.kportals.command;
 
+import org.kneelawk.kportals.KPortals;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.spec.CommandSpec;
+import org.spongepowered.api.text.Text;
+
 public class KPCommands {
 	public static void init() {
-		
+		CommandSpec rules = CommandSpec.builder().description(Text.of("Opens a test gui"))
+				.permission("kportals.command.rules").executor(new RulesCommandExecutor()).build();
+
+		CommandSpec kportals = CommandSpec.builder().description(Text.of("KPortals control command"))
+				.child(rules, "rules").build();
+
+		Sponge.getCommandManager().register(KPortals.getInstance(), kportals, "kportals", "kportal", "kp");
 	}
 }
--- src/main/java/org/kneelawk/kportals/command/RulesCommandExecutor.java
+++ src/main/java/org/kneelawk/kportals/command/RulesCommandExecutor.java
@@ -1,16 +1,26 @@
 package org.kneelawk.kportals.command;
 
+import org.kneelawk.kportals.KPortals;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.item.inventory.Inventory;
+import org.spongepowered.api.text.Text;
 
 public class RulesCommandExecutor implements CommandExecutor {
 
 	@Override
-	public CommandResult execute(CommandSource src, CommandContext args)
-			throws CommandException {
+	public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
+		if (src instanceof Player) {
+			Player player = (Player) src;
+			Inventory test = Inventory.builder().build(KPortals.getInstance());
+		} else {
+			src.sendMessage(Text.of("You must be a player to use the rulse gui"));
+			return CommandResult.empty();
+		}
 		return null;
 	}
 }
